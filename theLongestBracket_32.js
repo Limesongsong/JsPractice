54804e33ac218553eebc7bc30de846de0cead60d
‘limesongsong’ <lime_sss@163.com>
2022-11-30 21:07:20 +0800

    力扣76_32

--- theLongestBracket_32.js
+++ theLongestBracket_32.js
@@ -1,24 +1,21 @@
-var longestValidParentheses = function (s) {
-  let maxLen = 0
+var longestValidParentheses = function(s) {
+  let maxLen = 0;
+  let len = s.length
   let stack = []
-  stack.push(-1) // 初始化一个参照物
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === '(') {
-      // ( 入栈   )出栈
-      stack.push(i)
-    } else {
-      // )的情况 出栈
-      stack.pop()
-      if (stack.length) {
-        // 每次出栈 计算下当前有效连续长度
-        // 如何计算连续长度 当前位置 - 栈顶下标
-        maxLen = Math.max(maxLen, i - stack[stack.length - 1])
+  stack.push(-1)
+  for(let i = 0; i < len; i++) {
+      if(s.charAt(i) === '(') {
+          stack.push(i)
       } else {
-        stack.push(i) //栈为空时 放入右括号参照物 表示从这个下标开始 需要重新计算长度
+          stack.pop();
+          if(stack.length) {
+              maxLen = Math.max(maxLen, i - stack[stack.length - 1])
+          } else {
+              stack.push(i)
+          }
       }
-    }
   }
   return maxLen
-}
-console.log('result:', longestValidParentheses('(()))'))
+};
+console.log('result:', longestValidParentheses('()'))
 
